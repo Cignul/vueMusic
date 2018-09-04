6ccfab0cd229b151744d421740cbeef7473e5b70
Jackson Gordon <gordon.jacksonh@gmail.com>
2018-09-03 21:23:46 -0600

    trying to get the old app working with vue

--- client/src/main.js
+++ client/src/main.js
@@ -5,8 +5,18 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+class App {
+  constructor() {
+    this.controllers = {
+      itunesCtrl: new ItunesController()
+    }
+  }
+}
 new Vue({
   router,
   store,
   render: h => h(App)
 }).$mount('#app')
+import ItunesController from "./components/itunes-controller.js";
+
+
